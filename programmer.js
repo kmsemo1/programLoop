749c825f1e70b2c17ca6aeb61ee4b2f8aaf659be
kmsemo1 <32209919+kmsemo1@users.noreply.github.com>
2018-03-27 22:05:05 -0700

    added constructor adding prompt

--- programmer.js
+++ programmer.js
@@ -10,3 +10,22 @@ function Programmer(name, position, age, language){
         console.log("Name: " + this.name +"\nPosition: " + this.position + "\nAge: " + this.age + "\nLanguage: " + this.language);
     }
 }
+
+inquirer.prompt([
+    {
+        name:"name",
+        message: "Please type your name:"
+    },{
+        name:"position",
+        message:"What is your position?",
+    },{
+        name:"age",
+        message:"How old are you?" 
+    },{
+        name:"language",
+        message:"What langugage do you speak?"
+    }
+]).then(function(answers){
+    var newGuy = new Programmer(answers.name, answers.position, answers.age, answers.language);
+    newGuy.printInfo();
+})
