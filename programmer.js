60d9f2f5a86f833e7c1cc69fdc190b0ccbceedea
kmsemo1 <32209919+kmsemo1@users.noreply.github.com>
2018-03-28 16:38:33 -0700

    activity completed

--- programmer.js
+++ programmer.js
@@ -1,31 +1,45 @@
 var inquirer = require("inquirer");
 
-function Programmer(name, position, age, language){
+function Programmer(name, position, age, language) {
     this.name = name;
     this.position = position;
     this.age = age;
     this.language = language;
 
-    this.printInfo = function(){
-        console.log("Name: " + this.name +"\nPosition: " + this.position + "\nAge: " + this.age + "\nLanguage: " + this.language);
+    this.printInfo = function () {
+        console.log("Name: " + this.name + "\nPosition: " + this.position + "\nAge: " + this.age + "\nLanguage: " + this.language);
     }
 }
 
-inquirer.prompt([
-    {
-        name:"name",
-        message: "Please type your name:"
-    },{
-        name:"position",
-        message:"What is your position?",
-    },{
-        name:"age",
-        message:"How old are you?" 
-    },{
-        name:"language",
-        message:"What langugage do you speak?"
+var count = 0;
+
+var askQuestion = function () {
+    if (count < 2) {
+
+
+        inquirer.prompt([
+            {
+                name: "name",
+                message: "Please type your name:"
+            }, {
+                name: "position",
+                message: "What is your position?",
+            }, {
+                name: "age",
+                message: "How old are you?"
+            }, {
+                name: "language",
+                message: "What langugage do you speak?"
+            }
+        ]).then(function (answers) {
+            var newGuy = new Programmer(answers.name, answers.position, answers.age, answers.language);
+            newGuy.printInfo();
+            count++;
+            askQuestion();
+        })
+    } else {
+        console.log("Completed")
     }
-]).then(function(answers){
-    var newGuy = new Programmer(answers.name, answers.position, answers.age, answers.language);
-    newGuy.printInfo();
-})
+}
+
+askQuestion();
